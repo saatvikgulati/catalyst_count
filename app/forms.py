ece35b49f314aa2a1f47bc4ab6d77947cb5a9976
Saatvik Gulati <saatugulati@gmail.com>
2024-07-11 15:35:57 -0700

    aliasing

--- app/forms.py
+++ app/forms.py
@@ -1,38 +1,38 @@
-from django import forms
-from allauth.account.forms import SignupForm
-from app.models import UploadedFile, Company
+from django import forms as django_forms
+from allauth.account import forms as allauth_forms
+from app import models as app_models
 from django.contrib.auth.models import User
 
 
-class UploadForm(forms.ModelForm):
-    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}))
+class UploadForm(django_forms.ModelForm):
+    file = django_forms.FileField(widget=django_forms.FileInput(attrs={'class': 'custom-file-input'}))
 
     class Meta:
-        model = UploadedFile
+        model = app_models.UploadedFile
         fields = ['file']
 
 
-class AddUser(SignupForm):
-    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'First name'}))
-    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+class AddUser(allauth_forms.SignupForm):
+    first_name = django_forms.CharField(max_length=30, widget=django_forms.TextInput(attrs={'placeholder': 'First name'}))
+    last_name = django_forms.CharField(max_length=30, widget=django_forms.TextInput(attrs={'placeholder': 'Last name'}))
 
     class Meta:
         model = User
         fields = '__all__'
 
 
-class CompanyFilterForm(forms.Form):
-    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Name', 'class': 'col'}), required=False)
-    domain = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Domain', 'class': 'col'}), required=False)
-    year_founded = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Year founded'}), required=False)
-    industry = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Industry'}), required=False)
-    size_range = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Size range'}), required=False)
-    locality = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Locality'}), required=False)
-    country = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Country'}), required=False)
-    linkedin_url = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Linkedin url'}), required=False)
-    current_employee_estimate = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Current employee estimate'}), required=False)
-    total_employee_estimate = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Total employee estimate'}), required=False)
+class CompanyFilterForm(django_forms.Form):
+    name = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Name', 'class': 'col'}), required=False)
+    domain = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Domain', 'class': 'col'}), required=False)
+    year_founded = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Year founded'}), required=False)
+    industry = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Industry'}), required=False)
+    size_range = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Size range'}), required=False)
+    locality = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Locality'}), required=False)
+    country = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Country'}), required=False)
+    linkedin_url = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Linkedin url'}), required=False)
+    current_employee_estimate = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Current employee estimate'}), required=False)
+    total_employee_estimate = django_forms.CharField(max_length=255, widget=django_forms.TextInput(attrs={'placeholder': 'Total employee estimate'}), required=False)
 
     class Meta:
-        model = Company
+        model = app_models.Company
         fields = '__all__'
